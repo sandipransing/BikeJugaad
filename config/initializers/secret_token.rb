de4eb7810f076550fb13786332e8c662f99b7446
Sandip Ransing <sandip@joshsoftware.com>
2013-10-19 08:01:47 +0530

    rails new app using railswizard
    Devise  RSpec  HAML  Mongoid  Capybara  SASS  jQuery  Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentMyBikeIn::Application.config.secret_token = '549d28909f4d34352eb3e9f68902187cf9ec12cfe4d904471bff8b65e3018d759cec9fc28f1f74c833aff63fdef812b11e4688ac9eee9ff4a1a24438668b6a46'
