34b7cfb69622b7c2323e4ced7afac155ffc1d7de
adityashedge <aditya@joshsoftware.com>
2013-10-19 11:02:04 +0530

    added devise and links for sign_in/sign_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 RentMyBikeIn::Application.routes.draw do
+
+  root to: 'users#index'
+  devise_for :users
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
