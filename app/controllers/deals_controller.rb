fb24000a4ad026e73094e516291cba8f75423f8f
Sandip Ransing <sandip@joshsoftware.com>
2013-10-20 16:05:05 +0530

    deals search added

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -3,7 +3,16 @@ class DealsController < ApplicationController
   before_filter :load_user
 
   def search
-    @deals = Deal.asc(:from)
+    @deals =  Deal.asc(:from)
+    @deals = @deals.full_text_search(params[:city]) if params[:city].present?
+    case true
+    when params[:start].present? && params[:end].present?
+      @deals = @deals.where(:from.lte => params[:start], :till.gte => params[:end]) 
+    when params[:start].present?
+      @deals = @deals.where(:from.lte => params[:start])
+    when params[:end].present?
+      @deals = @deals.where(:till.gte => params[:end])
+    end
   end
 
   def index
