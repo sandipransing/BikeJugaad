7c498d1597f82b6e23f9a3b3017f66bb1e891bdc
adityashedge <aditya@joshsoftware.com>
2013-10-20 23:38:32 +0530

    user feedback and test case for deals

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def feedback
+    @feedback = params[:feedback]
+    UserMailer.send_feedback(@feedback).deliver
+    redirect_to contact_us_path
+  end
 end
--- spec/controllers/deals_controller_spec.rb
+++ spec/controllers/deals_controller_spec.rb
@@ -18,8 +18,53 @@ describe DealsController do
 
   describe 'GET #show' do
     it "should show deal for specified id" do
-      get :show, id: @deal
+      sign_in @user
+      xhr :get, :show, id: @deal
       expect(assigns(:deal)).to eq @deal
+    end
+  end
+
+  describe 'GET #new' do
+    it "should get new deal" do
+      sign_in @user
+      get :new
+      deal = assigns(:deal)
+      expect(deal.from).to eq(Date.today)
+      expect(deal.till).to eq(Date.today + 3.days)
+      expect(response).to render_template :new
+    end
+
+    it "should not get new deal" do
+      get :new
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe 'Get #edit' do
+    it "should get edit for specified deal" do
+      sign_in @user
+      get :edit, id: @deal
+      expect(assigns(:deal)).to eq @deal
+      expect(response).to render_template :edit
+    end
+
+    it "should not edit a deal" do
+      get :edit, id: @deal
+      expect(response).to redirect_to root_path
+    end
+  end
+
+  describe 'PUT #update' do
+    it "should update a specified deal" do
+      sign_in @user
+      put :update, id: @deal, deal: attributes_for(:deal, make: 'Hero')
+      @deal.reload
+      expect(assigns(:deal)).to eq(@deal)
+    end
+
+    it "should not update a deal" do
+      put :update, id: @deal, deal: attributes_for(:deal, till: (Date.today - 2.days))
+      expect(response).to redirect_to root_path
     end
   end
 end
